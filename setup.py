89899392141278cb8c879968519dc8774b28a080
Christian Neumann <cneumann@datenkarussell.de>
2012-07-15 18:00:17 +0200

    Move from setuptools to distutils2, fix packaging

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
-from setuptools import setup, find_packages
-from setuptools import Command
-import sys, os
+#!/usr/bin/env python
 
-here = os.path.abspath(os.path.dirname(__file__))
-README = open(os.path.join(here, 'README.rst')).read()
-CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+from setuptools import Command
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distribute_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup
+
 
 class PyTest(Command):
     user_options = []
@@ -17,34 +21,9 @@ class PyTest(Command):
         errno = subprocess.call([sys.executable, 'runtests.py'])
         raise SystemExit(errno)
 
-setup(name='kotti_dkbase',
-      version='0.1.0b1',
-      description="Event collection for Kotti sites",
-      long_description=README + '\n\n' + CHANGES,
-      classifiers=[
-        "Development Status :: 4 - Beta",
-        "Programming Language :: Python",
-        "Framework :: Pylons",
-        "Topic :: Internet :: WWW/HTTP",
-        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
-        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
-        "License :: OSI Approved :: BSD License",
-        ],
-      author='Christian Neumann',
-      author_email='cneumann@datenkarussell.de',
-      license='BSD License',
-      packages=['kotti_dkbase'],
-      package_data={'kotti_dkbase': [
-            'static/*',
-            'templates/*.pt',
-          ]},
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-        'Kotti >= 0.6.0b1',
-      ],
-      entry_points="""
-      # -*- Entry points: -*-
-      """,
-      cmdclass = {'test': PyTest},
-      )
+
+setup(
+    setup_requires=['d2to1'],
+    d2to1=True,
+    cmdclass = {'test': PyTest},
+)
