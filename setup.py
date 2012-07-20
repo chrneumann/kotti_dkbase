dfa50e268c238657b8fdd52ff9e2b903a1d68f45
Christian Neumann <cneumann@datenkarussell.de>
2012-07-20 12:26:35 +0200

    Move to setuptools packaging based on kotti's.

--- setup.py
+++ setup.py
@@ -1,31 +1,41 @@
-#!/usr/bin/env python
+import os
 
-from setuptools import Command, find_packages
+from setuptools import setup
+from setuptools import find_packages
 
+here = os.path.abspath(os.path.dirname(__file__))
 try:
-    from setuptools import setup
-except ImportError:
-    from distribute_setup import use_setuptools
-    use_setuptools()
-    from setuptools import setup
+    README = open(os.path.join(here, 'README.rst')).read()
+    CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+except IOError:
+    README = CHANGES = ''
 
+install_requires = [
+    'Kotti ==0.7a4',
+    'pyramid_zcml',
+    'zope.browserresource',
+    'psycopg2',
+]
 
-class PyTest(Command):
-    user_options = []
-    def initialize_options(self):
-        pass
-    def finalize_options(self):
-        pass
-    def run(self):
-        import sys,subprocess
-        errno = subprocess.call([sys.executable, 'runtests.py'])
-        raise SystemExit(errno)
-
-
-setup(
-    setup_requires=['d2to1'],
-    d2to1=True,
-    cmdclass = {'test': PyTest},
-    packages=find_packages(),
-    include_package_data=True,
-)
+setup(name='kotti_dkbase',
+      version='0.1.0b1',
+      description='Kotti setup and modifications for CMS projects of Datenkarussell.',
+      long_description='\n\n'.join([README, CHANGES]),
+      classifiers=[
+          'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
+          'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+          'Programming Language :: Python',
+          'Topic :: Internet :: WWW/HTTP',
+          'Development Status :: 4 - Beta',
+          'License :: OSI Approved :: BSD License',
+          'Framework :: Pylons',
+      ],
+      author='Christian Neumann',
+      author_email='cneumann@datenkarussell.de',
+      url='https://github.com/chrneumann/kotti_dkbase',
+      license="BSD3",
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=install_requires,
+      )
