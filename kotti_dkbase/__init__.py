b3a75aab1fb0f1fdf367307dfe1ba7a7e673a7c1
Christian Neumann <cneumann@datenkarussell.de>
2012-07-27 12:35:50 +0200

    Remove override of kotti's fanstatic resources.

--- kotti_dkbase/__init__.py
+++ kotti_dkbase/__init__.py
@@ -2,32 +2,6 @@ from pyramid.httpexceptions import HTTPError
 from pyramid.httpexceptions import HTTPNotFound
 from kotti_dkbase.views import error_view
 from kotti_dkbase.views import exception_decorator
-import kotti.static as ks
-from fanstatic import Resource, Library
-from js.bootstrap import bootstrap_css
-
-def setup_needed_group():
-    """
-    Modify the NeededGroups in kotti.static to not depend on
-    bootstrap_responsive.
-    """
-    lib_kotti_new = Library("kotti2", ks.lib_kotti.path)
-    for group in [ks.view_needed, ks.edit_needed]:
-        for i, item in enumerate(group.resources):
-            if item is ks.view_css:
-                new_base = Resource(lib_kotti_new,
-                                    "base.css",
-                                    depends=[bootstrap_css, ],
-                                    minified="base.min.css")
-                group.resources[i] = Resource(lib_kotti_new,
-                                              "view.css",
-                                              depends=[new_base, ],
-                                              minified="view.min.css")
-            elif item is ks.edit_css:
-                group.resources[i] = Resource(lib_kotti_new,
-                                              "edit.css",
-                                              depends=[bootstrap_css, ],
-                                              minified="edit.min.css")
 
 def includeme(config):
     config.include('pyramid_zcml')
@@ -50,4 +24,3 @@ def includeme(config):
         )
     config.add_static_view('static-kotti_dkbase', 'kotti_dkbase:static')
     config.override_asset('kotti', 'kotti_dkbase:kotti-overrides/')
-    setup_needed_group()
