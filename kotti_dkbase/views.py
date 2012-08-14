7941c4c99abc7b9b17f396e45cd8d41e7e6bf601
Christian Neumann <cneumann@datenkarussell.de>
2012-08-14 16:49:39 +0200

    Fix pep8 errors.

--- kotti_dkbase/views.py
+++ kotti_dkbase/views.py
@@ -3,6 +3,7 @@ from pyramid_mailer.message import Message
 from kotti.message import get_mailer
 from kotti import get_settings
 
+
 def mail_admin(msg):
     """
     Send given message to the admins.
@@ -12,9 +13,10 @@ def mail_admin(msg):
         recipients=[admin],
         subject=u'Server-Error',
         body=msg,
-        )
+    )
     mailer = get_mailer()
     mailer.send_immediately(message)
+
 
 def exception_decorator(view):
     """
@@ -23,6 +25,7 @@ def exception_decorator(view):
     def f(exception, request):
         return view(HTTPInternalServerError(str(exception)), request)
     return f
+
 
 def error_view(exception, request):
     should_mail = get_settings().get('kotti_dkbase.send_error_mails') == "True"
