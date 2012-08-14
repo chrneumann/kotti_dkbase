738fc28922e0807bd292c8257ac251f5f743c237
Christian Neumann <cneumann@datenkarussell.de>
2012-08-14 16:48:01 +0200

    Fix pep8 errors.

--- kotti_dkbase/__init__.py
+++ kotti_dkbase/__init__.py
@@ -3,6 +3,7 @@ from pyramid.httpexceptions import HTTPNotFound
 from kotti_dkbase.views import error_view
 from kotti_dkbase.views import exception_decorator
 
+
 def includeme(config):
     config.include('pyramid_zcml')
     config.load_zcml('configure.zcml')
@@ -10,17 +11,17 @@ def includeme(config):
         error_view,
         context=HTTPNotFound,
         renderer='kotti_dkbase:templates/view/error-404.pt',
-        )
+    )
     config.add_view(
         error_view,
         context=HTTPError,
         renderer='kotti_dkbase:templates/view/error.pt',
-        )
+    )
     config.add_view(
         error_view,
         decorator=exception_decorator,
         context=Exception,
         renderer='kotti_dkbase:templates/view/error.pt',
-        )
+    )
     config.add_static_view('static-kotti_dkbase', 'kotti_dkbase:static')
     config.override_asset('kotti', 'kotti_dkbase:kotti-overrides/')
